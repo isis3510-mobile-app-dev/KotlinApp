292840e2b0e821c27f91a2a7f74ff7fc9c3f867b
lauram354 <l.murciac@uniandes.edu.co>
2026-03-02 11:59:01 -0500

    Typography

--- app/src/main/java/com/example/petcare/ui/theme/Type.kt
+++ app/src/main/java/com/example/petcare/ui/theme/Type.kt
@@ -5,30 +5,43 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.petcare.R
+import androidx.compose.ui.text.font.Font
 
 // Set of Material typography styles to start with
+val RobotoBold = FontFamily(
+    Font(R.font.roboto_bold)
+)
+
+val RobotoMedium = FontFamily(
+    Font(R.font.roboto_medium)
+)
+
+val RobotoRegular = FontFamily(
+    Font(R.font.roboto_regular)
+)
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        fontFamily = RobotoMedium,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        fontFamily = RobotoBold,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp,
+        lineHeight = 33.8.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = RobotoRegular,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
     )
-    */
+
 )
