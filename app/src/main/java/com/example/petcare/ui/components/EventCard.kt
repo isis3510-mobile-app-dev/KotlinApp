cab842e7ab3da62270f14630c53c0eea927d554f
marialeang2 <ma.angulom1@uniandes.edu.co>
2026-03-03 17:22:33 -0500

    EventCard and VaccineCard

---
+++ app/src/main/java/com/example/petcare/ui/components/EventCard.kt
@@ -0,0 +1,105 @@
+package com.example.petcare.ui.components
+
+import android.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.LocalHospital
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Vaccines
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.sql.Date
+
+@Composable
+fun EventCard(eventName: String,pet:String, date: String, daysOverdue: Int){
+    Card(modifier = Modifier.fillMaxWidth().padding(16.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(modifier = Modifier.size(46.dp).clip(CircleShape).background(Color(0xFFD0BCFF)),
+                contentAlignment = Alignment.Center
+            ){
+                Icon(
+                    imageVector = Icons.Default.LocalHospital,
+                    contentDescription = "Event",
+                    tint = Color.Black
+                )
+            }
+            Spacer(modifier = Modifier.width(12.dp))
+            Column(modifier = Modifier.weight(1f)) {
+
+                Text(
+                    text = eventName,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Row(){
+                    Text (
+                        text = "$pet - ",
+                        fontSize = 14.sp,
+                        color = Color.Gray
+                    )
+
+                    Text (
+                        text = date,
+                        fontSize = 14.sp,
+                        color = Color.Gray
+                    )}
+            }
+            Box(
+                modifier = Modifier.clip(RoundedCornerShape(50.dp)).background(Color(0xFF9FF2E2)).padding(horizontal = 10.dp, vertical = 4.dp)
+
+            ){
+                Row(){
+                    Icon(
+                        imageVector = Icons.Default.Notifications,
+                        contentDescription = "Event",
+                        tint = Color.Black,
+                        modifier = Modifier.size(16.dp)
+                    )
+                    Text(
+                    text = "Reminder on",
+                    fontSize = 12.sp,
+                    color = Color(0xFF006A60),
+                    fontWeight = FontWeight.Medium)
+                }
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun EventCardPreview(){
+    EventCard(eventName ="Vet Visit",
+        pet = "Max",
+        date = "March 3, 2025",
+        daysOverdue = 349)
+}
